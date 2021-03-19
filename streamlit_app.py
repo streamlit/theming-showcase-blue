643848e722a7d5efb516adaa18f380824a81bfab
Johannes Rieke <johannes.rieke@gmail.com>
2021-03-19 19:28:26 +0100

    Link to apps page

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,8 @@ for col, theme in zip(cols, THEMES):
         border_color = "lightgrey" if IS_DARK_THEME else "black"
 
     col.markdown(
-        f'<p align=center><a href="https://share.streamlit.io/{GITHUB_OWNER}/{repo}/main"><img style="border: 1px solid {border_color}" alt="{theme}" src="https://raw.githubusercontent.com/{GITHUB_OWNER}/theming-showcase/main/thumbnails/{theme}.png" width=150></a></p>',
+        #f'<p align=center><a href="https://share.streamlit.io/{GITHUB_OWNER}/{repo}/main"><img style="border: 1px solid {border_color}" alt="{theme}" src="https://raw.githubusercontent.com/{GITHUB_OWNER}/theming-showcase/main/thumbnails/{theme}.png" width=150></a></p>',
+        f'<p align=center><a href="https://apps.streamlitusercontent.com/{GITHUB_OWNER}/{repo}/main/streamlit_app.py/+/"><img style="border: 1px solid {border_color}" alt="{theme}" src="https://raw.githubusercontent.com/{GITHUB_OWNER}/theming-showcase/main/thumbnails/{theme}.png" width=150></a></p>',
         unsafe_allow_html=True,
     )
     if theme in ["light", "dark"]:
